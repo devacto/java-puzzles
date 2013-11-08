71aff7d787e5a2f50bc2efb0a698eb1975118587
Victor Wibisono <devacto@gmail.com>
2013-11-08 21:57:33 +1100

    Initial commit.

---
+++ ConferencePlanner/src/com/devacto/conferenceplanner/FileFormatException.java
@@ -0,0 +1,30 @@
+package com.devacto.conferenceplanner;
+
+public class FileFormatException extends Exception {
+	
+	/**
+	 * Exception class to handle wrong string format
+	 * in the text file.
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1495775446306927166L;
+	
+	private String errorDescription;
+	private int lineNumber;
+
+	public FileFormatException(String errorDescription, int lineNumber) {
+		this.errorDescription = errorDescription;
+		this.lineNumber = lineNumber;
+	}
+
+	public String getErrorDescription() {
+		return errorDescription;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+	
+	
+}
