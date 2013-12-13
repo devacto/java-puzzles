7e7c1ad9183202ac93d8176d14dbccc3e79953bc
Victor Wibisono <devacto@gmail.com>
2013-12-14 10:22:01 +1100

    Added JUnit testing for Test class.

--- 01-conference-planner/src/ac/victor/java/conferenceplanner/_TalkTest.java
+++ 01-conference-planner/src/ac/victor/java/conferenceplanner/_TalkTest.java
@@ -3,10 +3,11 @@
  */
 package ac.victor.java.conferenceplanner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.junit.Test;
 
 /**
@@ -59,10 +60,21 @@ public class _TalkTest {
 	@Test
 	public void testCompareTo() {
 		// Create one talk with minute duration = 10.
+		Talk t1 = new Talk("T1", 10);
+		
 		// Create one talk with minute duration = 20.
+		Talk t2 = new Talk("T2", 20);
+		
+		// Put them into an array.
+		ArrayList<Talk> talkList = new ArrayList<Talk>(2);
+		talkList.add(t1);
+		talkList.add(t2);
+		
 		// Sort it.
+		Collections.sort(talkList);
+		
 		// Get the first item from the sorted list.
-		// The first item should be 20.
+		assertEquals(20, talkList.get(0).getMinuteDuration());
 	}
 
 }
