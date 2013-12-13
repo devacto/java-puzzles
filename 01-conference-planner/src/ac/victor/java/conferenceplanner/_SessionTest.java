6b60bf327939d40950f0f84ae44eda16f8e16299
Victor Wibisono <devacto@gmail.com>
2013-12-14 10:32:16 +1100

    Generated test stubs for the various methods in class Session.

--- 01-conference-planner/src/ac/victor/java/conferenceplanner/_SessionTest.java
+++ 01-conference-planner/src/ac/victor/java/conferenceplanner/_SessionTest.java
@@ -1,23 +1,55 @@
+/**
+ * 
+ */
 package ac.victor.java.conferenceplanner;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author victor
+ *
+ */
 public class _SessionTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	/**
+	 * Test method for {@link ac.victor.java.conferenceplanner.Session#Session(ac.victor.java.conferenceplanner.Talk, java.util.Date)}.
+	 */
 	@Test
-	public void test() {
+	public void testSession() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ac.victor.java.conferenceplanner.Session#setTalk(ac.victor.java.conferenceplanner.Talk)}.
+	 */
+	@Test
+	public void testSetTalk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ac.victor.java.conferenceplanner.Session#setStartTime(java.util.Date)}.
+	 */
+	@Test
+	public void testSetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ac.victor.java.conferenceplanner.Session#getStartTime()}.
+	 */
+	@Test
+	public void testGetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ac.victor.java.conferenceplanner.Session#getTalk()}.
+	 */
+	@Test
+	public void testGetTalk() {
 		fail("Not yet implemented");
 	}
 
