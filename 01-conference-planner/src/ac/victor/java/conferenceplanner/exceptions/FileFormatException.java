3f7e1fcb97ba3c993a02604d9f40b8924dfb9f5c
Victor Wibisono <devacto@gmail.com>
2013-12-14 15:34:28 +1100

    Minor modifications.

--- 01-conference-planner/src/ac/victor/java/conferenceplanner/exceptions/FileFormatException.java
+++ 01-conference-planner/src/ac/victor/java/conferenceplanner/exceptions/FileFormatException.java
@@ -5,7 +5,6 @@ public class FileFormatException extends Exception {
 	/**
 	 * Exception class to handle wrong string format
 	 * in the text file.
-	 * 
 	 */
 	
 	private static final long serialVersionUID = 1495775446306927166L;
