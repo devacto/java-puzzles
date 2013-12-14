17e95be2ce5dbf280ba698d0e32b104935f2bfe4
Victor Wibisono <devacto@gmail.com>
2013-12-15 09:36:36 +1100

    Test created for FileOutputProducerUtil class.

---
+++ 01-conference-planner/src/ac/victor/java/conferenceplanner/tests/FileOutputProducerUtilTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ac.victor.java.conferenceplanner.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This is the test suite  for 
+ * @author victor
+ *
+ */
+public class FileOutputProducerUtilTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
