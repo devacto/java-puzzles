9d488bf805b921121e678ba224801ab5d2d62231
Victor Wibisono <devacto@gmail.com>
2013-12-14 14:19:01 +1100

    Created tests for Track class.

---
+++ 01-conference-planner/src/ac/victor/java/conferenceplanner/tests/TrackTest.java
@@ -0,0 +1,24 @@
+package ac.victor.java.conferenceplanner.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrackTest {
+
+	@Test
+	public void testTrack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSession() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSessions() {
+		fail("Not yet implemented");
+	}
+
+}
