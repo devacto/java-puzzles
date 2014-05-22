100b764c75f317cafdb094939fc6471dd25a0d48
Victor Wibisono <devacto@gmail.com>
2014-05-22 11:00:45 +1000

    Added constants to old module.

---
+++ 01-conference-planner/src/ac/victor/java/conferenceplanner/Constants.java
@@ -0,0 +1,8 @@
+package ac.victor.java.conferenceplanner;
+
+/**
+ * Created by conduct11 on 13/05/14.
+ */
+public class Constants {
+    public static final String DEFAULT_FILE_NAME = "io/test.input";
+}
